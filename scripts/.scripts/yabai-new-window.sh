6cc3ff41153283e0e48837c7c7c0099257143d77
David Diosdado <david.diosdado@Davids-MacBook-Air.local>
2024-09-21 05:20:25 -0600

    Se arregló cambio de layout para que use index del space en vez de id

--- scripts/.scripts/yabai-new-window.sh
+++ scripts/.scripts/yabai-new-window.sh
@@ -2,6 +2,9 @@
 
 # Custom tiling rules for Inkscape and Gimp
 
+default_browser="Vivaldi"
+secondary_display="1"
+
 window=$(yabai -m query --windows --window)
 window_id=$(echo $window | jq '.id')
 
@@ -20,9 +23,13 @@ is_inkscape_other_conditions='
     (."app" | contains("Inkscape")) and
     (."title" | contains(" - Inkscape") | not)
 '
+is_browser_conditions="
+    (.\"app\" | contains(\"$default_browser\"))
+"
 echo $window | jq -er "$is_gimp_main_conditions" && is_gimp_main="1"
 echo $window | jq -er "$is_inkscape_main_conditions" && is_inkscape_main="1"
 echo $window | jq -er "$is_inkscape_other_conditions" && is_inkscape_other="1"
+echo $window | jq -er "$is_browser_conditions" && is_browser="1"
 
 # Only Inkscape and Gimp main windows should be tiled
 if [ "$is_gimp_main" -eq "1" ] || [ "$is_inkscape_main" -eq "1" ]; then
@@ -32,5 +39,15 @@ fi
 # Inkscape dialogues are resized and centered on the screen to force their redrawing
 if [ "$is_inkscape_other" -eq "1" ]; then
     yabai -m window $window_id --grid 7:5:1:1:3:5
+fi
+
+# Browser windows are sent to the secondary display by default
+if [ "$is_browser" -eq "1" ]; then
+    yabai -m space --focus "s_main"
+    total_displays=$(yabai -m query --displays | jq -r length)
+    if [ "$total_displays" -gt "1" ]; then
+        yabai -m window --display "$secondary_display"
+        yabai -m display --focus "$secondary_display"
+    fi
 fi
 
--- scripts/.scripts/yabai-toggle-layout.sh
+++ scripts/.scripts/yabai-toggle-layout.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-while getopts l: flag
+while getopts l:s: flag
 do
     case "${flag}" in
         l) new_layout=${OPTARG};;
+        s) space_index=${OPTARG};;
     esac
 done
+
 
 spaces=$(yabai -m query --spaces)
 space_current=$(echo $spaces | jq '.[] | select(."has-focus" == true)')
 layout_type=$(echo $space_current | jq -r '.type')
-space_id=$(echo $space_current | jq -r '.id')
+
+
 
 if [ -z "$new_layout" ]; then
     case "$layout_type" in
         bsp) new_layout=stack;;
         stack) new_layout=bsp;;
-        # float) new_layout=bsp;;
+        float) new_layout=bsp;;
     esac
 fi
 
-yabai -m space --layout $new_layout
-~/.scripts/notification.sh -m "$new_layout" -t "Yabai"
+
+if [ -z "$space_index" ]; then
+    space="Current Space"
+    yabai -m space --layout $new_layout
+else
+    space_data=$(echo $spaces | jq ".[] | select(.\"index\" == $space_index)")
+    space_label=$(echo $space_data | jq -r '.label')
+    space="Space: $space_index:$space_label"
+    yabai -m space "$space_index" --layout "$new_layout"
+fi
+
+~/.scripts/notification.sh -m "$space\rLayout: $new_layout" -t "Yabai"
+
+
 

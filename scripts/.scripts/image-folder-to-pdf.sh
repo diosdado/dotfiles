135f783dde756d5b384614390e9ad0da55f14632
David Diosdado <david.diosdado@Davids-MacBook-Air.local>
2024-11-07 18:11:37 -0600

    turn a collection of folders with images into a collection of pdfs

---
+++ scripts/.scripts/image-folder-to-pdf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OIFS="$IFS"
+IFS=$'\n'
+
+folders=$(find $1/ -mindepth 1 -maxdepth 1 -type d)
+
+for folder in $folders  ; do
+
+    folder_name=$(basename "$folder")
+    echo "Converting ${folder_name} to pdf..."
+    magick "${1}/${folder_name}/*.jpg" -auto-orient "${1}/${folder_name}.pdf"
+
+done
+
+IFS="$OIFS"
+

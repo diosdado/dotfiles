2718f8152efa14b4da38239ab50bde777d68cb80
David Diosdado <david.diosdado@Davids-MacBook-Air.local>
2024-11-07 18:13:19 -0600

    macos window borders script moved outside the yabai configuration

---
+++ scripts/.scripts/reset-borders.sh
@@ -0,0 +1,12 @@
+
+
+
+# start helper app to show borders around focused window
+borders                         \
+    style=round                 \
+    active_color=0xff00ff00     \
+    inactive_color=0x00ffffff   \
+    width=8.0                   \
+    hidpi=on                    \
+    blacklist="Stickies,Free\ Ruler,Reminders"
+

feaceba35a1358471da1b13aaa8a502657b4918b
David Diosdado <david.diosdado@Davids-MacBook-Air.local>
2024-11-07 19:08:08 -0600

    rewritten to handle multiple windows of the selected apps

--- scripts/.scripts/yabai-window-focused.sh
+++ scripts/.scripts/yabai-window-focused.sh
@@ -1,14 +1,26 @@
-window=$(echo $(yabai -m query --windows) | jq '.[] | select(."has-focus" == true)')
-window_reminders=$(echo $(yabai -m query --windows) | jq '.[] | select(."title" == "Reminders")')
+#!/bin/bash
 
-# yabai -m query --windows | jq '.[] | select(."app" == "Stickies" or ."app" == "Vivaldi") | .id'
+while getopts "r" flag
+do
+    case $flag in
+        r) reset="1";; # resets the opacity of the windows to 1
+    esac
+done
 
-window_id=$(echo $window | jq '.id')
-window_reminders_id=$(echo $window_reminders | jq '.id')
+focused=$(echo $(yabai -m query --windows) | jq '.[] | select(."has-focus" == true) | .id')
+windows=$(yabai -m query --windows | jq '.[] | select(."app" == "Stickies" or ."app" == "Reminders") | .id')
 
-if [[ $window_id -eq $window_reminders_id ]]; then
-    yabai -m window "$window_reminders_id" --opacity 0.0
-else
-    yabai -m window "$window_reminders_id" --opacity 0.4
-fi
+for win_id in $windows; do
+    if [[ "$focused" -eq "$win_id" || "$reset" -eq "1" ]]; then
+        yabai -m window "$win_id" --opacity 0.0
+    else
+        yabai -m window "$win_id" --opacity 0.4
+    fi
+done
+
+
+
+
+
+
 

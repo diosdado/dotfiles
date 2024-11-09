0cf207ecc2e18c2b5cec09cc6da806617661e1f8
David Diosdado <david.diosdado@Davids-MacBook-Air.local>
2024-11-08 19:12:17 -0600

    changed names of scripts

--- scripts/.scripts/kill-music-library-agent-and-eject.sh
+++ scripts/.scripts/kill-music-library-agent-and-eject.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# when the Apple Music Library is stored in an external volume, some agents prevent the ejection of said volume
+# this script kills these agents and ejects the volume called 'Music'
+
+
 killall AMPDevicesAgent 2>/dev/null
 killall AMPLibraryAgent 2>/dev/null
 diskutil unmount /Volumes/Music

6d4067cbec6f74b1a7c4756e503f988df426c7a6
David Diosdado <david.diosdado@Davids-MacBook-Air.local>
2024-11-17 04:11:24 -0600

    added shortcuts

--- nvim/.config/nvim/lua/diosdado/remap.lua
+++ nvim/.config/nvim/lua/diosdado/remap.lua
@@ -54,6 +54,7 @@ vim.keymap.set("n", "<leader>n", [[:e ~/Notes/.txt<Left><Left><Left><Left>]])
 
 -- make the current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>z", ":%s:\\r::g<CR>")
 
 -- source configuration
 vim.keymap.set("n", "<leader><leader>", function()

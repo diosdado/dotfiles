aefe1ccf5e3f487e3e6da19aa967a9bf6dd2dc54
David Diosdado <david.diosdado@diosdado-air.local>
2025-07-20 04:13:45 -0600

    limpieza

--- scripts/.scripts/yabai/layout-normal.sh
+++ scripts/.scripts/yabai/layout-normal.sh
@@ -3,7 +3,10 @@
 
 
 yabai -m rule --apply app="^kitty$"             space=s_primary
+sleep .1
 yabai -m rule --apply app="^Brave\ Browser$"    space=s_primary
+sleep .1
 yabai -m rule --apply app="^Inkscape$"          space=s_third
+sleep .1
 yabai -m rule --apply app="^GIMP$"              space=s_third
 

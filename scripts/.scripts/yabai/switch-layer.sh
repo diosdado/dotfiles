9b6a41dff0d97b206611ccdabd30e722c2dc34d7
David Diosdado <david.diosdado@diosdado-air.local>
2025-10-23 03:57:16 -0600

    se eliminó el cambio de variables con karabiner en los scripts de cambio de color de borde

--- scripts/.scripts/yabai/switch-layer.sh
+++ scripts/.scripts/yabai/switch-layer.sh
@@ -11,32 +11,22 @@ done
 
 case $layer in
     none)
-        '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --set-variables '{"yabai_layer":0}'
-        '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --set-variables '{"app_layer":0}'
         /opt/homebrew/bin/borders active_color=$border_inactive
         ;;
 
     default)
-        '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --set-variables '{"yabai_layer":0}'
-        '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --set-variables '{"app_layer":0}'
         /opt/homebrew/bin/borders active_color=$border_active
         ;;
 
     shift)
-        '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --set-variables '{"yabai_layer":0}'
-        '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --set-variables '{"app_layer":0}'
         /opt/homebrew/bin/borders active_color=$border_shift
         ;;
 
     yabai)
-        '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --set-variables '{"yabai_layer":1}'
-        '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --set-variables '{"app_layer":0}'
         /opt/homebrew/bin/borders active_color=$border_yabai
         ;;
 
     apps)
-        # '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --set-variables '{"yabai_layer":0}'
-        # '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --set-variables '{"app_layer":1}'
         /opt/homebrew/bin/borders active_color=$border_apps
         ;;
 esac
